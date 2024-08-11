76c5376a2a98a7b0e8b23a086b8ab6b67534a621
BuildTools <unconfigured@null.spigotmc.org>
2024-08-11 16:12:57 +0300

    basig game loop

---
+++ MedievalGameEngine/Engine/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedievalGameEngine.Engine
+{
+    public class Game
+    {
+        private bool isRunning;
+        private int targetFrameRate = 60;
+        public void Start()
+        {
+            isRunning = true;
+            Task.Run(GameLoop);
+        }
+
+        private async Task GameLoop()
+        {
+            while (isRunning)
+            {
+                DateTime start = DateTime.Now;
+
+                ProcessInput();
+                UpdateGameState();
+                await BroadCastGameState();
+
+                TimeSpan frameTime = DateTime.Now - start;
+                double sleepTime = (1000 / targetFrameRate) - frameTime.TotalMilliseconds;
+
+                if (sleepTime > 0)
+                {
+                    await Task.Delay((int)sleepTime);
+                }
+            }
+        }
+
+        private Task BroadCastGameState()
+        {
+            Console.WriteLine("Updated input");
+            return Task.CompletedTask;
+        }
+
+        private void UpdateGameState()
+        {
+            Console.WriteLine("Update");
+        }
+
+        private void ProcessInput()
+        {
+            Console.WriteLine("Process");
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+    }
+}
--- MedievalGameEngine/Program.cs
+++ MedievalGameEngine/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MedievalGameEngine.Engine;
+
 Console.WriteLine("Hello, World!");
+
+Game game = new Game();
+game.Start();
