f39e1b62f48ec1c29a20a759711db931042c31ba
BuildTools <unconfigured@null.spigotmc.org>
2024-08-11 17:18:12 +0300

    Basic EntityComponentSystem

---
+++ MedievalGameEngine/EntityComponentSystem/Core/EntityManager/EntityManager.cs
@@ -0,0 +1,33 @@
+﻿using MedievalGameEngine.EntityComponentSystem.Core.Entities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedievalGameEngine.EntityComponentSystem.Core.EntityManager
+{
+    public class EntityManager : IEntityManager
+    {
+        private Dictionary<Guid, Dictionary<Type, IComponent>> _entities = new();
+
+        public IEntity RegisterEntity(IEntity entity)
+        {
+            _entities[entity.Id] = new Dictionary<Type, IComponent>();
+            return entity;
+        }
+
+        public void AddComponent<T>(IEntity entity, T component) where T : IComponent
+        {
+            var components = _entities[entity.Id];
+            components[typeof(T)] = component;
+        }
+
+        public Dictionary<Type, IComponent> GetComponents(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MedievalGameEngine/EntityComponentSystem/Core/EntityManager/IEntityManager.cs
@@ -0,0 +1,19 @@
+﻿using MedievalGameEngine.EntityComponentSystem.Core.Entities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedievalGameEngine.EntityComponentSystem.Core.EntityManager
+{
+    public interface IEntityManager
+    {
+        public IEntity RegisterEntity(IEntity entity);
+
+        public void AddComponent<T>(IEntity entity, T component) where T : IComponent;
+
+        public Dictionary<Type, IComponent> GetComponents(IEntity entity);
+    }
+}
